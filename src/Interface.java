fa276e727eff4cffb7298941266cd550f951baa2
M3XH4 <mechasean23123@gmail.com>
2024-04-04 20:32:13 +0800

    Create: EXQuest Beta

--- src/Interface.java
+++ src/Interface.java
@@ -5,7 +5,6 @@ public class Interface {
     private Enemy enemy;
     private Random random;
     private Scanner input;
-
     boolean commandLoop = true;
     boolean spellLoop = true;
     boolean spellSelectLoop = true;
@@ -48,9 +47,105 @@ public class Interface {
             } else if (command.equalsIgnoreCase("Spell")) {
                 if (getPlayer().getGrimoire().getGrimoire().isEmpty()) {
                     System.out.println("Spirit Guide: Do You Want To Cast Spell? (Yes/No)");
+                    do {
+                        String spellAnswer = getInput().nextLine();
+                        if (spellAnswer.equalsIgnoreCase("Yes")) {
+                            System.out.println("Spirit Guide: What Spell Would You Like To Use: ");
+
+                            for(int i = 0; i < getPlayer().getGrimoire().getGrimoire().size(); i++) {
+                                if (i == 0) { System.out.print("("); }
+                                System.out.print(getPlayer().getGrimoire().getGrimoire().get(i).getSpellName());
+                                if (i != (getPlayer().getGrimoire().getGrimoire().size() - 1)) {System.out.print("/"); }
+                                else { System.out.println(")"); }
+                            }
+                            do {
+                                System.out.println("Your Response - ");
+                                String spell = getInput().nextLine();
+                                Spells selectedSpell = getPlayer().getGrimoire().getSpell(spell);
+                                if (selectedSpell != null) {
+                                    if (selectedSpell instanceof Fireball fireball) {
+                                        spellCastDisplay(fireball);
+                                        fireball.setFire();
+                                    } else if (selectedSpell instanceof Blizzard blizzard) {
+                                        spellCastDisplay(blizzard);
+                                    } else if (selectedSpell instanceof Thunder thunder) {
+                                        spellCastDisplay(thunder);
+                                    } else {
+                                        System.out.println("Spirit Guide: I Cannot Read The Spell You Are Casting. Are You Sure That\nIs One Of Your Choices. Please Cast A Spell Again");
+                                    }
+                                }
+                            } while(spellSelectLoop);
+                        }
+
+                    } while (spellLoop);
                 }
             }
         } while(commandLoop);
+    }
+    public void spellCastDisplay(Spells spells) {
+        boolean spellCommandLoop = true;
+        boolean castCommandLoop = true;
+        boolean readCommandLoop = true;
+        if (getPlayer().getMana() < spells.getManaNeeded()) {
+            System.out.println("Spirit Guide: You Have Insufficient Mana, Unable To Cast Spell.");
+            spellSelectLoop = false;
+            spellLoop = false;
+        } else {
+            do {
+                System.out.println("Spirit Guide: Do You Want To: (Cast/Read/Back)");                                                 
+                System.out.println("|Cast - Cast Spell|  |Read - Read Description of Spell|  |Back - Go Back To Spell Selection|");
+                System.out.print("Your Response - ");
+                String spellCommand = getInput().nextLine();
+                if (spellCommand.equalsIgnoreCase("Cast")) {
+                    System.out.println("Spirit Guide: Are You Sure You Want To Cast " + spells.getSpellName() + "? (Yes/No)");
+                    do {
+                        System.out.print("Your Response - ");
+                        String castCommand = input.nextLine();
+                        if (castCommand.equalsIgnoreCase("Yes"))  {
+                            spells.spellAttack(getPlayer(), getEnemy());
+                            castCommandLoop = false;
+                            spellCommandLoop = false;
+                            spellSelectLoop = false;
+                            spellLoop = false;
+                            commandLoop = false;
+                        } else if (castCommand.equalsIgnoreCase("No")) {
+                            castCommandLoop = false;
+                            spellCommandLoop = false;
+                        } else {
+                            confuseMessage(1);
+                        }
+                    } while (castCommandLoop);
+                } else if (spellCommand.equalsIgnoreCase("Read")) {
+                    spells.getSpellDescription();
+                    do {
+                        System.out.println("Spirit Guide: Do You Want To Cast This Spell? (Yes/No)");
+                        String readCommand = getInput().nextLine();
+                        if (readCommand.equalsIgnoreCase("Yes")) {
+                            readCommandLoop = false;
+                        } else if (readCommand.equalsIgnoreCase("No")) {
+                             readCommandLoop = false;
+                             spellCommandLoop = false;
+                        } else {
+                             confuseMessage(2);
+                        }
+                    } while(readCommandLoop);
+                } else if (spellCommand.equalsIgnoreCase("Back")) {
+                    spellCommandLoop = false;
+                } else {
+                    confuseMessage(2);
+                }                       
+
+            } while(spellCommandLoop);
+        }
+    }
+    public void winDisplay() {
+        System.out.println("Spirit Guide: You Defeated " + getEnemy().getName());
+        System.out.println("Spirit Guide: Congratulations!!!");
+        getPlayer().levelUp(getEnemy());
+    }
+    public void loseDisplay() {
+        System.out.println("Spirit Guide: You Have Been Defeated By " + getEnemy().getName());
+        System.out.println("Spirit Guide: Don't Worry Warrior, I Will Revive You");
     }
     public void confuseMessage(int msgNumber) {
         System.out.print("Spirit Guide: I Could NOt Understand What Your Saying Warrior " + getPlayer().getName());
