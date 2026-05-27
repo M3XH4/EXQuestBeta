26dc6ff8f08146437d4426bab0f84bb3631972f3
M3XH4 <mechasean23123@gmail.com>
2026-05-28 01:47:32 +0800

    refactor: fixed title

--- src/WebAdventureSession.java
+++ src/WebAdventureSession.java
@@ -5,7 +5,7 @@ import java.util.Locale;
 import java.util.Random;
 
 public class WebAdventureSession {
-    private static final String TITLE = "-------------------------------------------------------       EXQuest       -------------------------------------------------------";
+    private static final String TITLE = "-------------------------------------------------------       EXQuest       ------------------------------------------------------";
 
     private enum Mode {
         AWAITING_NAME,
